6400ce452632c368016819bf046bd59823913927
Josiah Ayres <josiah@lend.com.au>
2023-05-31 09:13:24 +1200

    Add package.json from repo

--- apps/mantine-test/src/app/app.tsx
+++ apps/mantine-test/src/app/app.tsx
@@ -2,14 +2,19 @@ import styled from '@emotion/styled';
 
 import NxWelcome from './nx-welcome';
 
+import { CloseButton } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+
 const StyledApp = styled.div`
   // Your style here
 `;
 
 export function App() {
+  const [opened, { toggle }] = useDisclosure();
   return (
     <StyledApp>
-      <NxWelcome title="mantine-test" />
+      {opened ? 'open' : 'close'}
+      <CloseButton variant="transparent" onClick={() => toggle()} />
     </StyledApp>
   );
 }
